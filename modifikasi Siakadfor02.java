f42dcede26990f318f41042021823680b3d2e76a
adityaragila <adityaragil2006@gmail.com>
2025-10-16 08:09:44 +0700

    Percobaan KafeDoWhile02

--- modifikasi Siakadfor02.java
+++ modifikasi Siakadfor02.java
@@ -1,13 +1,13 @@
-import java.util.Scanner;
+public import java.util.Scanner;
 
-public class SiakadFor02 { 
+public class Siakadfor02 { 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         double nilai, tertinggi = 0, terendah = 100;
         
-        int lulus = 0;      
-        int tidakLulus = 0; 
+        int lulus = 0;      // untuk menghitung mahasiswa yang lulus (nilai >= 60)
+        int tidakLulus = 0; // untuk menghitung mahasiswa yang tidak lulus (nilai < 60)
 
         for (int i = 1; i <= 10; i++) {
             System.out.println("Memasukkan nilai ke-" + i + ": ");
@@ -20,17 +20,21 @@ public class SiakadFor02 {
                 terendah = nilai;
             }
             
-            
+        
             if (nilai >= 60) {
-                lulus++;        
+                lulus++;        // tambah counter lulus
             } else {
-                tidakLulus++;   
+                tidakLulus++;   // tambah counter tidak lulus
             }
         }
         
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        
+        // OUTPUT TAMBAHAN untuk menampilkan hasil kelulusan:
         System.out.println("Jumlah mahasiswa yang lulus: " + lulus);
         System.out.println("Jumlah mahasiswa yang tidak lulus: " + tidakLulus);
     }
+} {
+    
 }
